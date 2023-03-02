49d499414ffa23d39efbdb2d0ac9698771b2ecad
abdalah <abood.99.10.osa@gmail.com>
2023-03-02 23:16:12 +0300

    edit1

--- js/main.js
+++ js/main.js
@@ -7,96 +7,93 @@
   Created: Colorlib
 ---------------------------------------------------------  */
 
-'use strict';
-
+"use strict";
 (function ($) {
-
-    /*------------------
+  /*------------------
         Preloader
     --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
+  $(window).on("load", function () {
+    $(".loader").fadeOut();
+    $("#preloder").fadeOut("slow");
+  });
 
-    /*------------------
+  /*------------------
         Background Set
     --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
+  $(".set-bg").each(function () {
+    var bg = $(this).data("setbg");
+    $(this).css("background-image", "url(" + bg + ")");
+  });
 
-    //Canvas Menu
-    $(".canvas__open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("active");
-        $(".offcanvas-menu-overlay").addClass("active");
-    });
+  //Canvas Menu
+  $(".canvas__open").on("click", function () {
+    $(".offcanvas-menu-wrapper").addClass("active");
+    $(".offcanvas-menu-overlay").addClass("active");
+  });
 
-    $(".offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("active");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
+  $(".offcanvas-menu-overlay").on("click", function () {
+    $(".offcanvas-menu-wrapper").removeClass("active");
+    $(".offcanvas-menu-overlay").removeClass("active");
+  });
 
-     //Search Switch
-     $('.search-switch').on('click',function() {
-        $('.search-model').fadeIn(400);
-    });
+  //Search Switch
+  $(".search-switch").on("click", function () {
+    $(".search-model").fadeIn(400);
+  });
 
-    $('.search-close-switch').on('click',function() {
-        $('.search-model').fadeOut(400,function() {
-            $('#search-input').val('');
-        });
+  $(".search-close-switch").on("click", function () {
+    $(".search-model").fadeOut(400, function () {
+      $("#search-input").val("");
     });
+  });
 
-    /*------------------
+  /*------------------
 		Navigation
 	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
-    });
+  $(".mobile-menu").slicknav({
+    prependTo: "#mobile-menu-wrap",
+    allowParentLinks: true,
+  });
 
-    /*------------------
+  /*------------------
         Accordin Active
     --------------------*/
-    $('.collapse').on('shown.bs.collapse', function () {
-        $(this).prev().addClass('active');
-    });
+  $(".collapse").on("shown.bs.collapse", function () {
+    $(this).prev().addClass("active");
+  });
 
-    $('.collapse').on('hidden.bs.collapse', function () {
-        $(this).prev().removeClass('active');
-    });
-    
-    /*--------------------------
+  $(".collapse").on("hidden.bs.collapse", function () {
+    $(this).prev().removeClass("active");
+  });
+
+  /*--------------------------
         Testimonial Slider
     ----------------------------*/
-    var testimonialSlider = $(".testimonial__slider");
-    testimonialSlider.owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: true,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: false
-    });
+  var testimonialSlider = $(".testimonial__slider");
+  testimonialSlider.owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: true,
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: false,
+  });
 
-    /*------------------
+  /*------------------
 		Magnific
 	--------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
+  $(".video-popup").magnificPopup({
+    type: "iframe",
+  });
 
-    /*--------------------------
+  /*--------------------------
         Select
     ----------------------------*/
-    $("select").niceSelect();
+  $("select").niceSelect();
 
-    /*--------------------------
+  /*--------------------------
         Datepicker
     ----------------------------*/
-    $( ".datepicker_pop" ).datepicker();
-
+  $(".datepicker_pop").datepicker();
 })(jQuery);
